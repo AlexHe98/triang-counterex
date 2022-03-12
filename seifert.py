61ffa583c0216bd9f70caf0e7d0652607be721b5
alex-he <mr.a.he98@gmail.com>
2022-03-12 14:11:29 +1000

    Refactor Seifert fibre code.

--- seifert.py
+++ seifert.py
@@ -4,76 +4,93 @@ Seifert fibre, where we know in advance that e belongs to a one-vertex
 triangulation of a small Seifert fibre space (i.e., a Seifert fibre space
 over S^2 with three exceptional fibres).
 """
+from enum import Enum, auto
 from regina import *
 from pachner import twoThree
 
 
-def fastNotFibre(ideal):
-    """
-    Given an ideal triangulation that results from pinching an edge e in a
-    one-vertex triangulation of a small Seifert fibre space, uses fast tests
-    to see whether we can verify that e is not isotopic to a Seifert fibre.
-
-    Currently, this routine attempts to find a strict angle structure in the
-    given ideal triangulation. If such an angle structure exists, then the
-    given manifold is hyperbolic, which means it could not have resulted from
-    drilling out a Seifert fibre; thus, this routine returns True in this
-    case. Otherwise, this routine returns False because it could not make a
-    conclusive decision.
-    """
-    return ideal.hasStrictAngleStructure()
+class Fibre(Enum):
+    UNKNOWN = auto()
+    NONFIBRE = auto()
+    EXCEPTIONAL = auto()
+    REGULAR = auto()
 
 
-def fastFibre(bounded):
+def isBlockedSFSOverDisc_(tri):
     """
-    Given a bounded triangulation that results from drilling out an edge e in
-    a one-vertex triangulation of a small Seifert fibre space, uses fast
-    tests to see whether we can verify that e is isotopic to a Seifert fibre.
+    Is the given triangulation a blocked Seifert fibre space over the disc?
 
-    Currently, this routine uses combinatorial recognition to try to prove
-    that the given bounded triangulation represents a Seifert fibre space
-    over the disc with either two or three exceptional fibres, which means
-    that filling the edge e back into the torus boundary would recover the
-    original Seifert fibre space with e as a fibre; thus, this routine
-    returns True in this case. Otherwise, this routine returns False because
-    it could not make a conclusive decision.
+    If this routine is able to determine that the answer is yes, then it
+    returns the number of exceptional fibres. If this routine is able to
+    determine that the answer is no, then it returns -1. This routine does
+    not guarantee that it will always come to one of these two conclusions,
+    and will raise a ValueError whenever it encounters an unsolved case.
     """
-    # Retriangulate with height 0, and check whether combinatorial
-    # recognition works for any of the resulting triangulations.
+    blocked = BlockedSFS.recognise(tri)
+    if blocked is None:
+        return False
+    sfs = blocked.manifold()
+    if sfs.name() == "M/n2 x~ S1":
+        # Homeomorphic to SFS [D: (2,1) (2,-1)].
+        return 2
+    if ( sfs.baseOrientable() and sfs.baseGenus() == 0 and
+            sfs.punctures() == 1 ):
+        # Base is a disc.
+        return sfs.fibreCount()
+    else:
+        # We have a blocked Seifert fibre space whose base is not a disc, but
+        # since we do not want to do any more work at this point, we simply
+        # mark this case as unsolved.
+        raise ValueError(
+                "Unsolved case: {}".format( sfs.detail().rstrip() ) )
+
+
+def isFibre(edge):
+    """
+    Assuming that the given edge belongs to a one-vertex triangulation of a
+    small Seifert fibre space, determines whether this edge is isotopic to a
+    Seifert fibre.
+    """
+    # Pinch the given edge. If the resulting ideal triangulation admits a
+    # strict angle structure, then the edge cannot be a Seifert fibre.
+    drilled = Triangulation3( edge.triangulation() )
+    drilled.pinchEdge( drilled.edge( edge.index() ) )
+    if drilled.hasStrictAngleStructure():
+        return Fibre.NONFIBRE
+
+    # Now truncate, and see whether we can prove that the edge is isotopic to
+    # a Seifert fibre using combinatorial recognition.
+    drilled.idealToFinite()
+    drilled.intelligentSimplify()
+    drilled.intelligentSimplify()
+    counts = []
     def recognise( sig, tri ):
-        blocked = BlockedSFS.recognise(tri)
-        if blocked is None:
-            return False
-        sfs = blocked.manifold()
-        # Verify that we have a Seifert fibre space over the disc with either
-        # two or three exceptional fibres.
-        differentReps = [
-                "M/n2 x~ S1" ] # Homeomorphic to SFS [D: (2,1) (2,-1)].
-        if sfs.name() in differentReps:
-            return True
-        elif ( sfs.baseOrientable() and sfs.baseGenus() == 0 and
-                sfs.punctures() == 1 and ( sfs.fibreCount() in {2,3} ) ):
+        # The following may raise a ValueError in an unsolved case.
+        fibreCount = isBlockedSFSOverDisc_(tri)
+        if fibreCount in {2,3}:
+            counts.append(fibreCount)
             return True
         else:
-            # TODO I'm not sure what to do in this case, so for now I'm just
-            #   going to raise an Exception.
-            raise ValueError(
-                    "Unexpected SFS: {}".format( sfs.detail().rstrip() ) )
+            return False
     height = 0
     nThreads = 1
-    return bounded.retriangulate( height, nThreads, recognise ) 
+    if drilled.retriangulate( height, nThreads, recognise ):
+        if len(counts) != 1:
+            raise ValueError( "Wrong number of counts: {}".format(counts) )
+        fibreCount = counts[0]
+        if fibreCount == 2:
+            return Fibre.EXCEPTIONAL
+        elif fibreCount == 3:
+            return Fibre.REGULAR
+        else:
+            raise ValueError(
+                    "Should never get {} exceptional fibres.".format(
+                        fibreCount ) )
 
-
-def annulusNotFibre( bounded ):
-    """
-    Given a bounded triangulation that results from drilling out an edge e in
-    a one-vertex triangulation of a small Seifert fibre space, uses essential
-    annuli to see whether we can verify that e is not isotopic to a Seifert
-    fibre.
-    """
-    # Search for annuli among the vertex normal surfaces.
+    # We need to try more powerful techniques: search for essential annuli
+    # and essential tori.
     # TODO
-    pass
+    return Fibre.UNKNOWN
 
 
 # Test code.
@@ -82,11 +99,11 @@ if __name__ == "__main__":
     tests = [
             ( "cPcbbbqxh", "SFS [S2: (2,1) (2,1) (2,-1)]" ),
             ( "dLQbccchhrw", "SFS [S2: (2,1) (2,1) (3,-2)]" ) ]
-    def genDrilled(sig):
+    def genEdges(sig):
         tri = Triangulation3.fromIsoSig(sig)
         # What are the edges currently in the triangulation?
         for i in range( tri.countEdges() ):
-            yield Triangulation3(tri), i, "e{}".format(i)
+            yield tri, i, "e{}".format(i)
         # What new edges can we introduce using 2-3 moves?
         for i in range( tri.countTriangles() ):
             result = twoThree( tri.triangle(i) )
@@ -97,33 +114,25 @@ if __name__ == "__main__":
     # Perform tests.
     for sig, name in tests:
         print( sig, name )
-        for drilled, i, name in genDrilled(sig):
-            drilled.pinchEdge( drilled.edge(i) )
+        for tri, i, name in genEdges(sig):
             msg = "    " + name + ": {}"
-            if fastNotFibre(drilled):
-                print( msg.format("Not a fibre!" ) )
-                continue
-            drilled.idealToFinite()
-            drilled.intelligentSimplify()
-            drilled.intelligentSimplify()
             try:
-                verifyFibre = fastFibre(drilled)
+                fibreType = isFibre( tri.edge(i) )
             except ValueError as err:
-                print( msg.format(err) )
+                print( msg.format( err ) )
             else:
-                if verifyFibre:
-                    print( msg.format("Fibre!") )
-                else:
-                    print( msg.format("Inconclusive...") )
-
+                print( msg.format( fibreType.name ) )
+                if fibreType is Fibre.UNKNOWN:
                     # Can we find annuli among the vertex normal surfaces?
-                    surfs = NormalSurfaces.enumerate( drilled, NS_STANDARD )
+                    bounded = Triangulation3(tri)
+                    bounded.pinchEdge( bounded.edge(i) )
+                    bounded.idealToFinite()
+                    bounded.intelligentSimplify()
+                    bounded.intelligentSimplify()
+                    surfs = NormalSurfaces.enumerate( bounded, NS_STANDARD )
                     for s in surfs:
-                        if not s.isOrientable():
-                            continue
-                        if s.countBoundaries() != 2:
-                            continue
-                        if s.eulerChar() == 0:
+                        if ( s.isOrientable() and s.eulerChar() == 0 and
+                                s.hasRealBoundary() ):
                             print( "        {} ...".format(
                                 s.detail()[:60] ) )
         print()
