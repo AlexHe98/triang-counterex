f448ad283f2da3a04eb2f2cd8155427b241d396a
alex-he <mr.a.he98@gmail.com>
2022-03-17 11:22:41 +1000

    Fix Seifert fibre code.

--- seifert.py
+++ seifert.py
@@ -17,40 +17,55 @@ class Fibre(Enum):
     REGULAR = auto()        # Curve that is a regular fibre.
 
 
-def isBlockedSFSOverDisc_(tri):
-    """
-    Is the given triangulation a blocked Seifert fibre space over the disc?
+class SFS(Enum):
+    UNKNOWN = auto()    # Unknown whether SFS or not.
+    NOTSFS = auto()     # Not an SFS.
+    NONDISC = auto()    # SFS, but not over disc.
+    DISCTWO = auto()    # SFS over disc; 2 exceptional fibres.
+    DISCTHREE = auto()  # SFS over disc; 3 exceptional fibres.
+    DISCOTHER = auto()  # SFS over disc; neither 2 nor 3 exceptional fibres.
 
-    If this routine is able to determine that the answer is yes, then it
-    returns the number of exceptional fibres. If this routine is able to
-    determine that the answer is no, then it returns -1. This routine does
-    not guarantee that it will always come to one of these two conclusions,
-    and will raise a ValueError whenever it encounters an unsolved case.
+
+def recogniseSFSOverDisc(tri):
+    """
+    Is the given triangulation a Seifert fibre space over the disc?
+
+    If tri is recognised as a Seifert fibre space, then this routine will
+    conclusively determine whether it has a Seifert fibration over the disc.
+
+    This routine relies exclusively on combinatorial recognition, so it will
+    never be able to prove that tri is not a Seifert fibre space (i.e., it
+    will never return SFS.NOTSFS).
     """
     blocked = BlockedSFS.recognise(tri)
     if blocked is None:
-        return False
+        return SFS.UNKNOWN
     sfs = blocked.manifold()
     if sfs.name() == "M/n2 x~ S1":
-        # Homeomorphic to SFS [D: (2,1) (2,-1)].
-        return 2
-    if ( sfs.baseOrientable() and sfs.baseGenus() == 0 and
+        # Homeomorphic to SFS [D: (2,1) (2,-1)]. Apart from fibred solid
+        # tori, this is the only bounded Seifert fibre space with non-unique
+        # Seifert fibration.
+        return SFS.DISCTWO
+    elif ( sfs.baseOrientable() and sfs.baseGenus() == 0 and
             sfs.punctures() == 1 ):
         # Base is a disc.
-        return sfs.fibreCount()
+        fc = sfs.fibreCount()
+        if fc == 2:
+            return SFS.DISCTWO
+        elif fc == 3:
+            return SFS.DISCTHREE
+        else:
+            return SFS.DISCOTHER
     else:
-        # We have a blocked Seifert fibre space whose base is not a disc, but
-        # since we do not want to do any more work at this point, we simply
-        # mark this case as unsolved.
-        raise ValueError(
-                "Unsolved case: {}".format( sfs.detail().rstrip() ) )
+        # We have a blocked Seifert fibre space whose base is not a disc.
+        return SFS.NONDISC
 
 
 def isFibre(edge):
     """
     Assuming that the given edge belongs to a one-vertex triangulation of a
-    small Seifert fibre space, determines whether this edge is isotopic to a
-    Seifert fibre.
+    small Seifert fibre space that is not also fibred over the projective
+    plane, determines whether this edge is isotopic to a Seifert fibre.
     """
     # Pinch the given edge. If the resulting ideal triangulation admits a
     # strict angle structure, then the edge cannot be a Seifert fibre.
@@ -64,48 +79,44 @@ def isFibre(edge):
     drilled.idealToFinite()
     drilled.intelligentSimplify()
     drilled.intelligentSimplify()
-    counts = []
+    classifications = []
     def recognise( sig, tri ):
-        try:
-            # May raise a ValueError in an unsolved case.
-            fibreCount = isBlockedSFSOverDisc_(tri)
-        except ValueError:
+        c = recogniseSFSOverDisc(tri)
+        if c is SFS.UNKNOWN:
+            # We don't know if we have a Seifert fibre space.
             return False
-        if fibreCount in {2,3}:
-            counts.append(fibreCount)
-            return True
-        else:
-            return False
+        # Since recogniseSFSOverDisc() can never prove that something is not
+        # a Seifert fibre space, we now know that this tri is definitely a
+        # Seifert fibre space.
+        classifications.append(c)
+        return True
     height = 0
     nThreads = 1
     if drilled.retriangulate( height, nThreads, recognise ):
-        if len(counts) != 1:
-            raise ValueError( "Wrong number of counts: {}".format(counts) )
-        fibreCount = counts[0]
-        if fibreCount == 2:
+        # The drilled triangulation is definitely a Seifert fibre space.
+        # Moreover, we know that if the drilled triangulation has a Seifert
+        # fibration over the disc, then we will have recognised this.
+        if len(classifications) != 1:
+            raise ValueError( "Wrong number of classifications: {}".format(
+                classifications ) )
+        c = classifications[0]
+        if c is SFS.DISCTWO:
+            # SFS over disc with 2 exceptional fibres. We must have drilled
+            # out an exceptional fibre.
             return Fibre.EXCEPTIONAL
-        elif fibreCount == 3:
+        elif c is SFS.DISCTHREE:
+            # SFS over disc with 3 exceptional fibres. We must have drilled
+            # out a regular fibre.
             return Fibre.REGULAR
+        elif c is SFS.NONDISC:
+            # SFS, but not over the disc. We must have drilled out a curve
+            # that is not isotopic to a Seifert fibre.
+            return Fibre.NONFIBRE
         else:
             raise ValueError(
-                    "Should never get {} exceptional fibres.".format(
-                        fibreCount ) )
+                    "Should never recognise something as {}.".format(c) )
 
-    # We need to try more powerful techniques: search for compressing discs
-    # and essential annuli.
-    # If we started with an edge isotopic to a fibre in a small Seifert fibre
-    # space, then after drilling we should have a Seifert fibre space S over
-    # the disc with either two or three exceptional fibres. Either way, S
-    # cannot have a compressing disc. Moreover:
-    #   --> if there are two exceptional fibres, then there will be an
-    #       essential annulus that cuts S into two solid tori; and
-    #   --> if there are three exceptional fibres, then there will be an
-    #       essential annulus that cuts S into a solid torus and a Seifert
-    #       fibre space over the disc with two exceptional fibres.
-    # Since we always start by ruling out the presence of compressing discs,
-    # the annulus that we cut along must follow the core of any new solid
-    # torus pieces that we cut off.
-    # TODO Use compressing discs to rule out "degenerate" cases.
+    # Fast tests have failed us. Try looking at normal surfaces.
     surfs = NormalSurfaces.enumerate( drilled, NS_STANDARD )
     annuli = []
     for s in surfs:
@@ -113,22 +124,34 @@ def isFibre(edge):
             continue
         euler = s.eulerChar()
         if euler == 1:
-            # The surface s is a disc. Check that it isn't a compressing disc
-            # by cutting along s and verifying that the torus boundary
-            # doesn't fall apart into a single sphere boundary.
+            # The surface s is a disc. Determine whether it is a compressing
+            # disc by cutting along s and checking whether the torus boundary
+            # gets compressed down to a single sphere boundary.
             compress = s.cutAlong()
+            compress.intelligentSimplify()
+            compress.intelligentSimplify()
             if ( compress.countBoundaryComponents() == 1 and
-                    compress.boundaryComponent(0).eulerChar == 2 ):
+                    compress.boundaryComponent(0).eulerChar() == 2 ):
                 return Fibre.NONFIBRE
         elif euler == 0:
             annuli.append(s)
     if not annuli:
-        # The annulus that we are looking for doesn't exist.
+        # The drilled triangulation has no essential annuli, so we cannot
+        # have drilled out a Seifert fibre.
         return Fibre.NONFIBRE
+    # We are looking for an annulus that either:
+    #   --> cuts the drilled triangulation into two solid tori (in which case
+    #       we know that we drilled out an exceptional fibre); or
+    #   --> cuts the drilled triangulation into a solid torus and a Seifert
+    #       fibre space over the disc with 2 exceptional fibres (in which
+    #       case we possibly cannot determine whether we drilled out a
+    #       Seifert fibre).
+    # If we never find such an annulus, then we definitely did not drill out
+    # a Seifert fibre.
     for a in annuli:
-        # Try cutting along the annulus. If this is the annulus we are
-        # looking for, then we must get two pieces, at least one of which is
-        # a solid torus.
+        # Try cutting along the annulus a. If this causes the drilled
+        # triangulation to fall apart into two solid tori, then we know that
+        # we drilled out an exceptional fibre.
         cut = a.cutAlong()
         if cut.countComponents() != 2:
             # Cutting along a only yields one piece, so it isn't the annulus
@@ -149,26 +172,41 @@ def isFibre(edge):
             continue
 
         # Getting to this point means that cutting along a yields two pieces,
-        # exactly one of which is a solid torus. We might not be able to
-        # certify fibres here...
+        # exactly one of which is a solid torus. Is the other piece a Seifert
+        # fibre space over the disc with 2 exceptional fibres? First try to
+        # answer this using combinatorial recognition.
         other = notSolidTorus[0]
-        def recognise2( sig, tri ):
-            try:
-                # May raise a ValueError in an unsolved case.
-                fibreCount = isBlockedSFSOverDisc_(tri)
-            except ValueError:
-                return False
-            return ( fibreCount == 2 )
+        classifications = []
         height = 0
         nThreads = 1
-        if other.retriangulate( height, nThreads, recognise2 ):
-            # TODO Can we make this more definitive?
-            return Fibre.UNKNOWN
+        if other.retriangulate( height, nThreads, recognise ):
+            # The other piece is definitely a Seifert fibre space. Moreover,
+            # we know that if the other piece has a Seifert fibration over
+            # the disc, then we will have recognised this.
+            if len(classifications) != 1:
+                raise ValueError(
+                        "Wrong number of classifications: {}".format(
+                            classifications ) )
+            c = classifications[0]
+            if c is SFS.DISCTWO:
+                # SFS over disc with 2 exceptional fibres. We possibly cannot
+                # conclude anything.
+                return Fibre.UNKNOWN
+            elif c is SFS.DISCTHREE:
+                # SFS over disc with 3 exceptional fibres.
+                # TODO I'm not sure what to do here.
+                pass
+            elif c is SFS.NONDISC:
+                # SFS, but not over the disc.
+                # TODO I'm not sure what to do here.
+                pass
+            else:
+                raise ValueError(
+                        "Should never recognise something as {} {}.".format(
+                            c, "after cutting" ) )
 
-        # We need to resort to searching for an annulus again. This time, we
-        # are looking specifically for an annulus that cuts the other piece
-        # into two solid tori.
-        cutSurfs = NormalSurfaces.enumerate( notSolidTorus[0], NS_STANDARD )
+        # Now try looking at normal surfaces in the other triangulation.
+        cutSurfs = NormalSurfaces.enumerate( other, NS_STANDARD )
         cutAnnuli = []
         hasCompress = False
         for s in cutSurfs:
@@ -176,12 +214,15 @@ def isFibre(edge):
                 continue
             euler = s.eulerChar()
             if euler == 1:
-                # The surface s is a disc. Check that it isn't a compressing
-                # disc by cutting along s and verifying that the boundary is
-                # still just a torus.
+                # The surface s is a disc. Determine whether it is a
+                # compressing disc by cutting along s and checking whether
+                # the torus boundary gets compressed down to a single sphere
+                # boundary.
                 compress = s.cutAlong()
+                compress.intelligentSimplify()
+                compress.intelligentSimplify()
                 if ( compress.countBoundaryComponents() == 1 and
-                        compress.boundaryComponent(0).eulerChar == 2 ):
+                        compress.boundaryComponent(0).eulerChar() == 2 ):
                     hasCompress = True
                     break
             elif euler == 0:
@@ -208,7 +249,7 @@ def isFibre(edge):
                     onlySolidTori = False
                     break
             if onlySolidTori:
-                # Can we make this more definitive?
+                # TODO We possibly cannot say anything more definitive.
                 return Fibre.UNKNOWN
 
     # Surviving to this point means that the annulus we were looking for
@@ -220,12 +261,12 @@ def isFibre(edge):
 if __name__ == "__main__":
     # Generate test triangulations.
     tests = [
-            ( "cPcbbbqxh", "SFS [S2: (2,1) (2,1) (2,-1)]" ),
-            ( "dLQbccchhrw", "SFS [S2: (2,1) (2,1) (3,-2)]" ),
-            ( "eLAkbccddemken", "SFS [S2: (2,1) (2,1) (2,1)]" ),
-            ( "eLAkbccddemkij", "SFS [S2: (2,1) (2,1) (3,-1)] : #1" ),
-            ( "eLPkbcddddrwos", "SFS [S2: (2,1) (2,1) (3,-1)] : #2" ),
-            ( "eLMkbcdddhhhqx", "SFS [S2: (2,1) (2,1) (4,-3)]" ),
+#            ( "cPcbbbqxh", "SFS [S2: (2,1) (2,1) (2,-1)]" ),
+#            ( "dLQbccchhrw", "SFS [S2: (2,1) (2,1) (3,-2)]" ),
+#            ( "eLAkbccddemken", "SFS [S2: (2,1) (2,1) (2,1)]" ),
+#            ( "eLAkbccddemkij", "SFS [S2: (2,1) (2,1) (3,-1)] : #1" ),
+#            ( "eLPkbcddddrwos", "SFS [S2: (2,1) (2,1) (3,-1)] : #2" ),
+#            ( "eLMkbcdddhhhqx", "SFS [S2: (2,1) (2,1) (4,-3)]" ),
             ( "eLPkbcdddhrrnk", "SFS [S2: (2,1) (3,1) (3,-2)]" ) ]
     def genEdges(sig):
         tri = Triangulation3.fromIsoSig(sig)
